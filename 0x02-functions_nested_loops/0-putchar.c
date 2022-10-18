a648c592ef0ca3da2247e3ef511d148defc518d5
Emmanuel DJIDAGBAGBA <emmanueldjidagbagba@gmail.com>
2022-10-18 22:48:09 +0100

    update print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,20 +2,18 @@
 
 /**
  * main - Prints the _putchar
- * 
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char pc[] = "_putchar";
 	int i;
-	
+
+	char pc[] = "_putchar";
 	for (i = 0; pc[i] != '\0'; i++)
 	{
 		_putchar(pc[i]);
 	}
-
 	_putchar('\n');
-	
+
 	return (0);
 }
