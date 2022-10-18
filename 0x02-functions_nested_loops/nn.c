6f9dc1ec70d7cb73396f1b944f0e155a5c11cda9
Emmanuel DJIDAGBAGBA <emmanueldjidagbagba@gmail.com>
2022-10-19 00:45:11 +0100

    nature

--- 0x02-functions_nested_loops/nn.c
+++ 0x02-functions_nested_loops/nn.c
@@ -8,41 +8,41 @@
  */
 void print_times_table(int n)
 {
-    int x, y, z;
+    int row, column, product;
 
     if (n >= 0 && n <= 14)
     {
-        for (x = 0; x <= n; x++)
+        for (row = 0; row <= n; row++)
         {
-            for (y = 0; y <= n; y++)
+            for (column = 0; column <= n; column++)
             {
-                z = x * y;
-                if (z > 99)
+                product = row * column;
+                if (product > 99)
                 {
                     _putchar(',');
                     _putchar(32);
-                    _putchar((z / 100) + '0');
-                    _putchar(((z / 10) % 10) + '0');
-                    _putchar((z % 10) + '0');
+                    _putchar((product / 100) + '0');
+                    _putchar(((product / 10) % 10) + '0');
+                    _putchar((product % 10) + '0');
                 }
-                else if (z > 9)
+                else if (product > 9)
                 {
                     _putchar(',');
                     _putchar(32);
                     _putchar(32);
-                    _putchar(((z / 10) % 10) + '0');
-                    _putchar((z % 10) + '0');
+                    _putchar(((product / 10) % 10) + '0');
+                    _putchar((product % 10) + '0');
                 }
                 else
                 {
-                    if (y != 0)
+                    if (column != 0)
                     {
                         _putchar(',');
                         _putchar(32);
                         _putchar(32);
                         _putchar(32);
                     }
-                    _putchar(z + '0');
+                    _putchar(product + '0');
                 }
             }
             _putchar('\n');
