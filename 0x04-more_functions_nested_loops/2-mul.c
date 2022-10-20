dad59fc692655a528ca5a7d3d42a81b9c12f4759
Emmanuel DJIDAGBAGBA <emmanueldjidagbagba@gmail.com>
2022-10-20 23:18:58 +0100

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: integer first value
+ * @b: integer second value
+ * Return: mul of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
