180d99cb49e7043ec2714a807df1befe3125b32e
Emmanuel DJIDAGBAGBA <emmanueldjidagbagba@gmail.com>
2022-10-27 17:33:04 +0100

    toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters to uppercase
+ * @s: string 
+ *
+ * Return: address of s
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; *(s + i); i++)
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+			*(s + i) -= 'a' - 'A';
+	}
+	return (s);
+}
