63dbfcb3fd2a8473f9b943795b0858523a5136b7
Emmanuel DJIDAGBAGBA <emmanueldjidagbagba@gmail.com>
2022-10-27 17:39:11 +0100

    toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,12 +8,13 @@
  */
 char *string_toupper(char *s)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; *(s + i); i++)
+	while(*(s + i))
 	{
 		if (*(s + i) >= 'a' && *(s + i) <= 'z')
 			*(s + i) -= 'a' - 'A';
+		i++;
 	}
 	return (s);
 }
