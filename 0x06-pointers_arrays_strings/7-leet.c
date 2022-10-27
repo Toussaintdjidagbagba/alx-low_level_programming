48efecebd11c68341f76ba52799ca3295b3f1754
Emmanuel DJIDAGBAGBA <emmanueldjidagbagba@gmail.com>
2022-10-27 17:46:42 +0100

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,13 +8,13 @@
  */
 char *leet(char *s)
 {
-	int i, j;
+	int i = 0, j = 0;
 	char a[] = "aAeEoOtTlL";
 	char b[] = "4433007711";
 
-	for (i = 0; *(s + i); i++)
+	for (; *(s + i); i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (; j <= 9; j++)
 		{
 			if (a[j] == *(s + i))
 				*(s + i) = b[j];
