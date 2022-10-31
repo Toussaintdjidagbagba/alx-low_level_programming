8e4da4bda34e30d133bc055e59463e7e8b066ed2
Emmanuel DJIDAGBAGBA <emmanueldjidagbagba@gmail.com>
2022-10-31 16:05:32 +0100

    print diagsum and set string

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
-* print_diagsums -  prints the sum of the two diagonals of a square matrix of integers.
+* print_diagsums - prints diagonals sum
 *
 * @a: Pointer to an array
 *
@@ -13,15 +13,15 @@
 void print_diagsums(int *a, int size)
 {
 	int i;
-	int b = 0;
 	int c = 0;
+	int d = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		b += a[i];
-		c += a[size - i - 1];
+		c += a[i];
+		d += a[size - i - 1];
 		a += size;
 	}
-	printf("%d, ", b);
-	printf("%d\n", c);
+	printf("%d, ", c);
+	printf("%d\n", d);
 }
