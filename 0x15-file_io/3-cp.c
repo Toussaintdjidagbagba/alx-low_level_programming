3eb722dc21a890a690eef03f4ba7c01ded3f75c7
Emmanuel DJIDAGBAGBA <emmanueldjidagbagba@gmail.com>
2022-12-05 21:00:07 +0100

    copy file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,7 +4,7 @@ int copy(char *file_from, char *file_to);
 void close_file(int fd);
 
 /**
-* main - calls copy function
+* main - calls copy function, exits if command line args are not up to 3
 *
 * @argc: command line argument count
 * @argv: array of command line argument vectors
@@ -37,15 +37,15 @@ int main(int argc, char *argv[])
 
 int copy(char *file_from, char *file_to)
 {
-	int file_f, file_t, rd_frm, wr_to;
+	int file_d1, file_d2, rd_frm, wr_to;
 	char *BUFF;
 
 	BUFF = malloc(1024 * sizeof(char));
 
-	file_f = open(file_from, O_RDONLY);
-	file_t = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	file_d1 = open(file_from, O_RDONLY);
+	file_d2 = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 
-	if (file_f == -1)
+	if (file_d1 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 		free(BUFF);
@@ -53,18 +53,18 @@ int copy(char *file_from, char *file_to)
 	}
 	else
 	{
-		rd_frm = read(file_f, BUFF, 1024);
-		wr_to = write(file_t, BUFF, rd_frm);
+		rd_frm = read(file_d1, BUFF, 1024);
+		wr_to = write(file_d2, BUFF, rd_frm);
 
-		if (wr_to == -1 || file_t == -1 || BUFF == NULL)
+		if (wr_to == -1 || file_d2 == -1 || BUFF == NULL)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 			free(BUFF);
 			exit(99);
 		}
 
-		close_file(file_f);
-		close_file(file_t);
+		close_file(file_d1);
+		close_file(file_d2);
 
 		free(BUFF);
 	}
