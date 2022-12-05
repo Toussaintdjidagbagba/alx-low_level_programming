3daba1e3f258d49b1f18a2ce3dba80ddbd4108fc
Emmanuel DJIDAGBAGBA <emmanueldjidagbagba@gmail.com>
2022-12-05 20:32:11 +0100

    append file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* append_text_to_file - appends text at the end of a file
+* @filename: name of file to append text into
+* @text_content: text to append to file
+* Return: 1 on success and -1 on failure
+*/
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int nf;
+
+	nf = open(filename, O_RDWR | O_APPEND);
+
+	if (nf == -1)
+		return (-1);
+	else
+		if (text_content != NULL)
+		{
+			int lntext = strlen(text_content);
+
+			write(nf, text_content, lntext);
+		}
+
+	return (1);
+}
