cb207caaff6501dd473f9b4c7a1d4ce2583a39fc
Emmanuel DJIDAGBAGBA <emmanueldjidagbagba@gmail.com>
2023-01-13 00:39:03 +0100

    update betty in 1-dlistint_len

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * dlistint_len - returns the number of elements in a 
+ * dlistint_len - returns the number of elements in a
  * linked dlistint_t list.
  * @h: pointer to the list.
  * Return: number of nodes.
